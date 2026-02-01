cc0eb5e3ea42d2572d48f628f9de8a0ac8c32dd7
shamika <shamanne.sawalha@gmail.com>
2026-02-01 17:24:17 +0800

    removed debug statements

--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,6 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import Intro from './components/Intro.jsx'
-import Middle from './components/Middle.jsx'
-import MidScroll from './components/MidScroll.jsx'
-import PostScroll from './components/PostScroll.jsx'
-import Distribution from './components/Distribution.jsx'
 
 
 createRoot(document.getElementById('root')).render(
